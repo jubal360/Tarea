5f2758968f5a78244894b00395f741c21ececc24
Jubal <jubalhackett@gmail.com>
2022-02-17 22:09:08 -0600

    Tarea numero 1

---
+++ src/main/java/com/Quality/Quality/QualityApplication.java
@@ -0,0 +1,13 @@
+package com.Quality.Quality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Quality/Quality/QualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Quality.Quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
